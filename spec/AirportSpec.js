64c7a3ae4f95db17bb710d6b130532e064af9f5e
James <jamesdix@cwgsy.net>
2016-12-07 09:53:06 +0000

    weather successfully stubbed

---
+++ spec/AirportSpec.js
@@ -0,0 +1,48 @@
+describe("Airport:", function() {
+
+  var airport;
+
+  beforeEach(function() {
+    airport = new Airport();
+    plane = jasmine.createSpy('plane')
+  });
+
+  describe("when the weather is NOT stormy:", function() {
+
+    describe("accepts a plane to land and:", function() {
+
+      it("should return confirmation", function() {
+        expect(airport.landPlane(plane)).toEqual('A plane has landed!')
+      });
+
+      it("should add a plane to the grounded fleet", function() {
+        airport.landPlane(plane);
+        expect(airport.planeCount()).toEqual(1)
+      });
+    });
+
+    describe("allows a plane to take off and:", function() {
+
+      it("should return a confirmation", function() {
+        expect(airport.takeOffPlane(plane)).toEqual("A plane has taken off!")
+      });
+
+      it("should minus a plane from the grounded fleet", function() {
+        airport.landPlane(plane);
+        airport.takeOffPlane(plane);
+        expect(airport.planeCount()).toEqual(0)
+      });
+    });
+  });
+
+  describe("when the weather IS stormy", function() {
+
+    beforeEach(function() {
+      spyOn(airport, 'IsStormy').and.returnValue(true)
+    });
+
+    it("a plane is refused landing", function() {
+      expect(function() {airport.landPlane(plane)}).toThrow(new Error('Stormy weather preventing landing!'))
+    });
+  });
+});
---
+++ src/Airport.js
@@ -0,0 +1,32 @@
+var Airport = function() {
+
+  this.planePopulation = [];
+  this.weatherCondition = 'Fair'
+
+};
+
+Airport.prototype.landPlane = function(plane) {
+  if(this.IsStormy()) {
+    throw new Error('Stormy weather preventing landing!');
+  } else {
+  this.planePopulation.push(plane);
+  return 'A plane has landed!';
+  }
+};
+
+Airport.prototype.takeOffPlane = function(plane) {
+  this.planePopulation.pop();
+  return 'A plane has taken off!';
+};
+
+Airport.prototype.planeCount = function() {
+  return this.planePopulation.length;
+};
+
+Airport.prototype.IsStormy = function() {
+  if(this.weatherCondition == "Stormy") {
+    return true;
+  } else {
+    return false;
+  }
+};
